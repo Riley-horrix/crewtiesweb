de1a3af2fd4980223302394f9a5175becdc1c917
rileyhorrix <rh1122@ic.ac.uk>
2024-08-07 07:46:43 +0100

    feat: Added content to README.md

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,14 +2,13 @@ import Image from "next/image"
 
 interface Props {
   size: number,
-  id?: number,
   className?: string
 }
 
 const aspect = 0.2647;
 
-export default function Logo({ size, id = 0, className = "" }: Props) {
+export default function Logo({ size, className = "" }: Props) {
   return (
-    <Image src={"/logo.png"} alt={"logo"} width={size} height={size * aspect} />
+    <Image className={className} src={"/logo.png"} alt={"logo"} width={size} height={size * aspect} />
   )
 }
